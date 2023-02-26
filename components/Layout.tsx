e8994dc8ee6d71167668fb5bc6334aa4a9414358
daifoll <kobykhnov.bl@gmail.com>
2023-02-26 21:15:55 +0700

    Набросал скелет страницы товара, доработал Layout (теперь header, основная часть и footer имеют отступы)

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,13 @@
+import { GetStaticProps } from 'next'
 import React from 'react'
 
-export default function Layout({ children }: ILayout ){
-  return (
-    <>  
-        <header>HEADER</header>
-        <main>{children}</main>
-        <footer>FOOTER</footer>
-    </>
-  )
+export default function Layout({ children }: ILayout) {
+    return (
+        <div className='h-screen flex flex-col justify-between'>
+            <header>HEADER</header>
+            <main>{children}</main>
+            <footer>FOOTER</footer>
+        </div>
+    )
 }
+
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,13 +1,56 @@
+import Layout from '@/components/Layout'
+import { GetStaticProps, InferGetStaticPropsType } from 'next'
 import { useRouter } from 'next/router'
 import React from 'react'
 
-export default function Product() {
-const router = useRouter()
-const {id} = router.query
+export default function Product({ product }: InferGetStaticPropsType<typeof getStaticProps>) {
 
-console.log(id)
- 
-return (
-    <div>{id}</div>
-  )
+    return (
+        <Layout>
+            <div>
+                <div className="flex basis-1/3" key={product.id}>
+                    <div className="w-80 flex flex-wrap justify-center">
+                        <img className="basis-full h-64 object-cover" src={product.images[0]} />
+                        <img className="basis-1/2 w-20 h-40 object-cover" src={product.images[1]} />
+                        <img className="basis-1/2 w-20 h-40 object-cover" src={product.images[2]} />
+                    </div>
+                    <div>
+                        <div><span>{product.category.name}</span></div>
+                        <div><strong>{product.title}</strong></div>
+                        <p>{product.description}</p>
+                        <button>BUY</button>
+                        <button>ADD TO CART</button>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    )
+}
+
+
+// This function gets called at build time
+export async function getStaticPaths() {
+    // Call an external API endpoint to get posts
+    const res = await fetch('https://api.escuelajs.co/api/v1/products')
+    const products = await res.json()
+
+    // Get the paths we want to pre-render based on posts
+    const paths = products.map((product: IProduct) => ({
+        params: { id: product.id.toString() },
+    }))
+
+    // We'll pre-render only these paths at build time.
+    // { fallback: false } means other routes should 404.
+    return { paths, fallback: false }
+}
+
+export const getStaticProps: GetStaticProps<{ product: IProduct }> = async ({ params }) => {
+    // const id = params?.id!
+
+    const res = await fetch(`https://api.escuelajs.co/api/v1/products/${params?.id}`)
+    const product = await res.json()
+
+    return {
+        props: { product }
+    }
 }
