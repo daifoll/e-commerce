68c2f3d77c39f75e8be85d54d9282497c6763991
daifoll <kobykhnov.bl@gmail.com>
2023-03-09 00:43:36 +0700

    Начал описывать проект

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,7 +3,22 @@ import Layout from "@/components/Layout"
 export default function About() {
     return (
         <Layout>
-            <div>Страница "О проекте"</div>
+            <p>
+                Проект представляет собой интернет-магазин (e-commerce) с возможностью выбора категории/продукта,
+                поиска товара, добавления товара в корзину, а также тестовой оплаты с помощью сервиса <a href="https://stripe.com" target="_blank">Stripe.</a>
+            </p>
+            <p>
+                <h1>Стек технологий:</h1>
+                <ul>
+                    <li>Next.js</li>
+                    <li>Typescript</li>
+                    <li>TailwindCSS</li>
+                    <li><a href="https://stripe.com" target="_blank">Stripe API</a></li>
+                </ul>
+            </p>
+            <p>
+                <h2>Описание работы</h2>
+            </p>
         </Layout>
     )
 }
