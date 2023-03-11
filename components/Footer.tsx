b383a0ab1b2a4181a60d950abe469201e93ab545
daifoll <kobykhnov.bl@gmail.com>
2023-03-12 01:58:20 +0700

    изменил контакты

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -23,10 +23,7 @@ export default function Footer({ categories }: IFooterProps) {
                 <h3 className="text-2xl font-medium uppercase mb-4">Контакты</h3>
                 <ul className='flex flex-col items-start h-auto'>
                     <li className='hover:text-stone-50 text-lg mb-3'><a href="mailto:kobykhnov.bl@gmail.com">kobykhnov.bl@gmail.com</a></li>
-                    <li className='hover:text-stone-50 text-lg mb-3'><a href="https://t.me/+79965003886" target="_blank">Telegram</a></li>
-                    {/* <li className='hover:text-stone-50 uppercase text-lg mb-3'><Link href={'/about'}>О проекте</Link></li>
-                    <li className='hover:text-stone-50 uppercase text-lg mb-3'><Link href={'/contacts'}>Контакты</Link></li>
-                    <li className='hover:text-stone-50 uppercase text-lg mb-3'><Link href={'/cart'}>Корзина</Link></li> */}
+                    <li className='hover:text-stone-50 text-lg mb-3'><a href="https://github.com/daifoll/e-commerce" target="_blank">GitHub</a></li>
                 </ul>
             </nav>
         </div>
