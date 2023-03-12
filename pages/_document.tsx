a8fa648bcabf50ea7478b846612f3d1ea6ae2a33
daifoll <kobykhnov.bl@gmail.com>
2023-03-12 20:30:30 +0700

    Добавил favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head title='Store - Next.js | Stripe | Typescript | Tailwind | Redux Toolkit pet-project'/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png"/>
       <body>
         <Main />
         <NextScript />
