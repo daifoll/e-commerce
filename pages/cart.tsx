3378bd8f129156621e36470f89a2c18158367556
daifoll <kobykhnov.bl@gmail.com>
2023-04-03 14:48:06 +0700

    Адаптировал cart page

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -81,33 +81,33 @@ export default function Cart() {
 
     return (
         <Layout>
-            <h1 className="text-3xl font-medium uppercase">Корзина</h1>
+            <h1 className="text-xl md:text-3xl font-medium uppercase">Корзина</h1>
             {
                 cartProducts.cartReducer?.products.length ?
-                    <div className="flex items-start mt-4">
-                        <div className="basis-4/6">
+                    <div className="flex flex-col md:flex-row items-start mt-4">
+                        <div className="basis-4/6 w-full md:w-auto">
                             <ul>
                                 {
                                     cartProducts.cartReducer.products.map((product) => (
                                         <li key={product.id + new Date().getDate()}>
-                                            <div className="flex bg-green-200 p-5 mb-3 rounded-2xl">
-                                                <div className="w-40">
+                                            <div className="flex flex-col sm:flex-row bg-green-200 p-5 mb-3 rounded-2xl">
+                                                <div className="w-full h-32 sm:h-auto sm:w-40">
                                                     <img className="w-full h-full object-cover" src={product.image} alt={product.title} />
                                                 </div>
-                                                <div className="ml-6">
-                                                    <p className="text-2xl"><Link href={`/product/${product.id}`}>{product.title}</Link></p>
-                                                    <p className="text-xl mt-5"> Кол-во:</p>
-                                                    <div className="flex items-center mt-1">
-                                                        <button onClick={() => handleClickDecrementQuantity(product)} className="text-3xl mr-2">
+                                                <div className="ml-0 sm:ml-6 mt-3 sm:mt-0">
+                                                    <p className="text-xl md:text-2xl"><Link href={`/product/${product.id}`}>{product.title}</Link></p>
+                                                    <p className="text-sm ms:text-xl mt-1 sm:mt-5"> Кол-во:</p>
+                                                    <div className="flex items-center mt-0 sm:mt-1">
+                                                        <button onClick={() => handleClickDecrementQuantity(product)} className="text-xl md:text-3xl mr-2">
                                                             <AiFillMinusCircle />
                                                         </button>
-                                                        <span className="text-3xl">{product.quantity}</span>
-                                                        <button onClick={() => handleClickIncrementQuantity(product)} className="text-3xl ml-2">
+                                                        <span className="text-xl md:text-3xl">{product.quantity}</span>
+                                                        <button onClick={() => handleClickIncrementQuantity(product)} className="text-xl md:text-3xl ml-2">
                                                             <AiFillPlusCircle />
                                                         </button>
                                                     </div>
-                                                    <div className="mt-5"><strong className="text-3xl">{getTotalPrice(product.id)}$</strong></div>
-                                                    <button className="text-base mt-4 p-2 font-semibold uppercase bg-red-400 hover:bg-red-300 text-white rounded-full" onClick={() => handleClickDeleteProduct(product)}>Удалить из корзины</button>
+                                                    <div className="mt-5"><strong className="text-xl md:text-3xl">{getTotalPrice(product.id)}$</strong></div>
+                                                    <button className="text-xs md:text-base mt-1 sm:mt-4 p-2 font-semibold uppercase bg-red-400 hover:bg-red-300 text-white rounded-full" onClick={() => handleClickDeleteProduct(product)}>Удалить из корзины</button>
                                                 </div>
                                             </div>
                                         </li>
@@ -115,17 +115,17 @@ export default function Cart() {
                                 }
                             </ul>
                         </div>
-                        <div className="sticky top-[110px] basis-2/6 ml-4 mb-3  bg-green-200 rounded-2xl p-5 focus:outline-none">
-                            <p className="font-semibold text-2xl">Итого: {cartProducts.cartReducer.total}$</p>
+                        <div className="sticky top-[110px] basis-2/6 w-full md:w-auto ml-0 md:ml-4 mb-3  bg-green-200 rounded-2xl p-5 focus:outline-none">
+                            <p className="font-semibold text-xl md:text-2xl">Итого: {cartProducts.cartReducer.total}$</p>
                             <form action="/api/checkout_sessions" method="POST" onSubmit={handleSubmit}>
                                 <label htmlFor='validateStatus'>
-                                    <input value={email} onChange={handleInputEmailChange} id="validateStatus" className="text-xl font-medium rounded-2xl p-2 focus:outline-none mt-2" name='email' placeholder="Введите e-mail" />
+                                    <input value={email} onChange={handleInputEmailChange} id="validateStatus" className="text-base md:text-xl w-[70%] md:w-auto font-medium rounded-2xl p-2 focus:outline-none mt-2" name='email' placeholder="Введите e-mail" />
                                     {error && <h2 className="text-red-500 font-medium mt-3 ml-2">{error}</h2>}
                                 </label>
 
                                 <input name='products' type='hidden' value={JSON.stringify(cartProductsStripe)} />
-                                <div className="mt-8">
-                                    <button type="submit" role="link" className="uppercase font-bold text-base p-2 px-4 bg-white hover:bg-gray-100 text-black rounded-full">Перейти к оплате</button>
+                                <div className="mt-3 md:mt-8">
+                                    <button type="submit" role="link" className="uppercase font-bold text-xs md:text-base p-2 px-4 bg-white hover:bg-gray-100 text-black rounded-full">Перейти к оплате</button>
                                 </div>
                             </form>
                         </div>
