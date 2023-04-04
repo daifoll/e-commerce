e57f8f742a6d03b0b00702fa5b267ae02cf79da0
daifoll <kobykhnov.bl@gmail.com>
2023-04-04 23:11:38 +0700

    небольшие исправления

--- pages/index.tsx
+++ pages/index.tsx
@@ -55,16 +55,14 @@ export default function Home({ categories, error }: InferGetStaticPropsType<type
             error ? <ErrorFetch error={error} />
               :
               categories.map((cat: ICategory, index) => {
-                const loader = () => {
-                  return `${cat.image}`
-                }
+
                 if (index > 4) {
                   return null
                 } else {
                   return <div key={cat.id} className="basis-full sm:basis-full md:basis-1/2 p-1 mt-8 sm:mt-5">
                     <Link aria-label={cat.name} href={`/category/${cat.id}/0?sortBy=default`} className="uppercase text-sm extra-sm:text-lg">{cat.name}</Link>
-                    <div className="w-full flex overflow-hidden">
-                      <Link aria-label={cat.name} className="hover:scale-[1.1] w-full transition-all" href={`/category/${cat.id}/0?sortBy=default`}><Image loader={loader} unoptimized={true} priority={true} width={300} height={300} className="w-full h-64 object-cover" src={cat.image ? cat.image : '/stubimg/notfound.png'} onError={(e) => hanlerOnErrorImage(e)} alt={cat.name} /></Link>
+                    <div className="w-full flex overflow-hidden h-64">
+                      <Link aria-label={cat.name} className="relative hover:scale-[1.1] w-full transition-all" href={`/category/${cat.id}/0?sortBy=default`}><Image unoptimized={true} priority={true} fill={true} className="object-cover" src={cat.image ? cat.image : '/stubimg/notfound.png'} onError={(e) => hanlerOnErrorImage(e)} alt={cat.name} /></Link>
                     </div>
                   </div>
                 }
